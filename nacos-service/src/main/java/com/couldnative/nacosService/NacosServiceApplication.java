2d01455126b68401d30daeb74ba802baedabcc5d
Thematrixofink <2495760174@qq.com>
2024-03-16 17:56:46 +0800

    创建父级项目

---
+++ nacos-service/src/main/java/com/couldnative/nacosService/NacosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.couldnative.nacosService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosServiceApplication.class, args);
+	}
+
+}
---
+++ nacos-service/src/test/java/com/couldnative/nacosService/NacosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couldnative.nacosService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
